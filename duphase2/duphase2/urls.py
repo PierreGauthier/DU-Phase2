ef1845aab10b2977955db1c0782d8cf5662ced70
pierregauthier <p_gauthier@live.fr>
2014-12-02 18:46:12 +0000

    django work on c9

--- duphase2/duphase2/urls.py
+++ duphase2/duphase2/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from presphase2 import views
+
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'duphase2.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', views.home),
     url(r'^admin/', include(admin.site.urls)),
 )
