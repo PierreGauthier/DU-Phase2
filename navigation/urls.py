7e2a4610c9d278c004784d136a9f0384233547a4
PierreGauthier <p_gauthier@live.fr>
2014-12-02 19:07:05 +0100

    Revert "Revert "Revert "copy of regital"""
    This reverts commit 48c336d38b728487398fd52c75ac14d1e9367f89.

--- navigation/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-
-from navigation import views
-
-urlpatterns = patterns('',
-	url(r'login/', views.login, name='login'),
-	url(r'logout/', views.logout, name='logout'),
-	url(r'', views.index, name='index'),
-)
--- stats/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-from stats import views
-
-urlpatterns = patterns('',
-  # .../saisie/
-  url(r'^$', views.statsForm),   
-	)
