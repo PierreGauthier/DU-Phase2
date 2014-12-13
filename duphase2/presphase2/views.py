da7d2562a368c7c2d00f10d286aed072f6bc8169
pierregauthier <p_gauthier@live.fr>
2014-12-13 00:38:37 +0000

    scroll menu

--- duphase2/presphase2/views.py
+++ duphase2/presphase2/views.py
@@ -9,11 +9,12 @@ def home(request):
     x=1366
         
     # (x,y,id,fichier svg,scale (%), rotation (deg), popup info, color, "left|right|middle", "content")
-    data.append((3,4,'car','cartop.svg',0.20,70,'<h3>titre</h3><p>Salut</a>','#63cdff','left sidebar very wide ', 'test'))
-    data.append((5,10,'cone','cone.svg',0.50,0,'<h3>plot</h3>','#ff8d2a','left sidebar very wide ', 'test'))
-    data.append((5,9,'cone2','cone.svg',0.50,0,'<h3>plot</h3>','#ff8d2a','large modal', '<h1>test<h1>'))
-    data.append((4,8,'cone3','cone.svg',0.50,0,'<h3>plot</h3>','#ff8d2a','right sidebar', 'test'))
-        
+    data.append((1,4,'car1','cartop.svg',0.20,60,'<h3>titre</h3><p>Salut</a>','#63cdff','left sidebar very wide ', 'test'))
+    data.append((4,12,'car5','cartop.svg',0.20,170,'<h3>titre</h3><p>Salut</a>','#556233','right sidebar very wide ', 'test'))
+    data.append((4,10,'cone1','cone.svg',0.5,0,'<h3>titre</h3><p>Salut</a>','#ff8d2a','modal ', 'test'))
+    data.append((4,9,'cone2','cone.svg',0.5,0,'<h3>titre</h3><p>Salut</a>','#ff8d2a','modal ', 'test'))
+    data.append((3,8,'cone3','cone.svg',0.5,0,'<h3>titre</h3><p>Salut</a>','#ff8d2a','modal ', 'test'))
+    
     return render_to_response('page_choixconcept.html', {'title':'Phase 2', 
     'data1':data, 'bg1':'street1lowres.jpg',
     'x':x,'xdiv2':x/2
